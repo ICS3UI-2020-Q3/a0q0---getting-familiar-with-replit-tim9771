d206f2f610345902eb8700a6c8e0fc69dc5511c5
tim9771 <weint9771@wrdsb.ca>
2021-02-18 16:50:38 +0000

    Created the Hello Person program

--- Main.java
+++ Main.java
@@ -1,15 +1,40 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The Hello World Program
+ * @author Tim Weinhardt
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Ask the user for their name
+    System.out.println("Please enter your name:");
+
+    //creating a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // declare and initialize the firstName variable
+    String firstName = input.nextLine();
+
+    // say hello to the user
+    System.out.println("Hello " + firstName);
+
+    // ask the user when they were born
+    System.out.println("Please enter the year you were born:");
+
+    // create the variable to store the birth year
+    int birthYear = input.nextInt();
+
+    // calculate the approximate age
+    final int CURRENT_YEAR = 2021;
+    int age = CURRENT_YEAR - birthYear;
+
+    // tell the user their age
+    System.out.println("You are approximately " + age + " years old.");
+
   }
 }
